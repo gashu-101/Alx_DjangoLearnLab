e16638e9e9b88363d337c1562b08c377d61f114c
Gashahun Woldeyohannes <gashahun.w1@gmail.com>
2024-09-01 19:12:22 +0300

    api endpoint

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
 from .views import BookList
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+]
+
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
 ]
