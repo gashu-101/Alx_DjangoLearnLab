1e27b950800ce72e16c4e1dd3f9bce5bef031dbb
Gashahun Woldeyohannes <gashahun.w1@gmail.com>
2024-09-21 16:51:32 +0300

    new commit

---
+++ django-models/relationship_app/query_samples.py
@@ -0,0 +1,9 @@
+from .models import *
+
+# Querying the database
+
+books_by_author =  Book.objects.filter(Author='Gashu')
+print(books_by_author)
+books_in_library = Library.objects.all()
+print(books_in_library)
+librarian = Librarian.objects.all()
