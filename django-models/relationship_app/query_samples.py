eae8aa7620c4be04ce055701fd8ee8be443b1b53
Gashahun Woldeyohannes <gashahun.w1@gmail.com>
2024-09-21 17:01:31 +0300

    new commit

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -1,16 +1,16 @@
-from .models import *
+from .models import Author, Book, Library, Librarian
 
-# Querying the database
+# Query all books by a specific author
+author_name = "Gashu"
+books_by_author = Book.objects.filter(author__name=author_name)
+print(f"Books by {author_name}: {books_by_author}")
 
-# Filter books by author's name (fixed field name and query)
-books_by_author = Book.objects.filter(author__name='Gashu')
-print(books_by_author)
+# List all books in a specific library
+library_name = "Some Library Name"  # Replace with actual library name
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+print(f"Books in {library_name}: {books_in_library}")
 
-# Get all books in a specific library (corrected get method)
-library_name = "Some Library Name"  # Define your library name here
-books_in_library = Library.objects.get(name=library_name).books.all()  # Fixed the get method
-print(books_in_library)
-
-# List all librarians
-librarians = Librarian.objects.all()
-print(librarians)
+# Retrieve the librarian for a specific library
+librarian = Librarian.objects.get(library=library)
+print(f"Librarian of {library_name}: {librarian}")
