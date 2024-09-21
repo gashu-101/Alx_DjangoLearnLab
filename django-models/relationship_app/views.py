5b6a369e04c56222a65958b54d270ab8bba62cb3
Gashahun Woldeyohannes <gashahun.w1@gmail.com>
2024-09-21 17:30:35 +0300

    last_commit

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,6 @@
-# relationship_app/views.py
-
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library  # Ensure Library is imported
 
 # Function-Based View to List All Books
 def list_books(request):
