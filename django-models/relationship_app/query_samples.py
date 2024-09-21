26b3412605a76768f7ea2dc0f4dc51d2475ff930
Gashahun Woldeyohannes <gashahun.w1@gmail.com>
2024-09-21 16:58:22 +0300

    new commit

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -8,7 +8,7 @@ print(books_by_author)
 
 # Get all books in a specific library
 library_name = "Some Library Name"  # Define your library name here
-books_in_library = Library.objects.get(name=library_name).books.all()
+books_in_library = Library.objects.get(library_name).books.all()
 print(books_in_library)
 
 # List all librarians
