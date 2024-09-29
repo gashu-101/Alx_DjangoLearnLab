1c20408445c4243743986323ff722560683ab6b3
Gashahun Woldeyohannes <gashahun.w1@gmail.com>
2024-09-29 23:23:10 +0300

    new commit

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,11 +1,11 @@
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import  permissions
 from .models import Post
 from .serializers import PostSerializer
 
 class FeedView(generics.ListAPIView):
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
