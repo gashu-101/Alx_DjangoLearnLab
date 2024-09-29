cd26c753b1bd79151e1561094fd5e9a0beb12fc6
Gashahun Woldeyohannes <gashahun.w1@gmail.com>
2024-09-29 23:37:08 +0300

    new commit

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import LikePostView, UnlikePostView
 
 urlpatterns = [
-    path('posts/<int:post_id>/like/', LikePostView.as_view(), name='like-post'),
-    path('posts/<int:post_id>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
+    path('posts/<int:pk>/like/', LikePostView.as_view(), name='like-post'),
+    path('posts/<int:pk>/unlike/', UnlikePostView.as_view(), name='unlike-post'),
 ]
