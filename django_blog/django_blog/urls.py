b16ec26294096dd29630e60bff2e0ea87807afb7
Gashahun Woldeyohannes <gashahun.w1@gmail.com>
2024-10-19 16:56:07 +0300

    new commit

--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -2,7 +2,7 @@
 URL configuration for django_blog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,15 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .views import register_view, login_view, logout_view, profile_view
-
-
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('profile/', profile_view, name='profile'),
+    path('', include('blog.urls')),
 ]
+
+
